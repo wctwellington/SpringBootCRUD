6456d3a4d164139d8b9065323ebc1a61be4bb456
Wellington <wctwellington@bol.com.br>
2018-01-19 17:42:35 -0200

    First commit

---
+++ SpringBootCRUD/src/main/java/com/springbootcrud/DataConfiguration.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.springbootcrud;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+/**
+ *
+ * @author wellington
+ */
+@Configuration
+public class DataConfiguration {
+    
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/springboot");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("123");
+        return dataSource;
+    }
+    
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.POSTGRESQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQL9Dialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
+    }
+    
+}
